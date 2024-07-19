2aba13e4f2fdc260a18753479618341037aacc41
daeun084 <030804jk@naver.com>
2024-07-19 22:08:48 +0900

    refactor: 막차 시간 계산 로직 리팩토링

--- src/main/java/makar/dev/manager/MakarManager.java
+++ src/main/java/makar/dev/manager/MakarManager.java
@@ -59,241 +59,136 @@ public class MakarManager {
         return takingTime;
     }
 
+    // 환승이 없는 경우 막차 시간 구하기
     public Calendar computeLastMakarTime(int dayOfWeek, SubwaySchedule subwaySchedule, int odsayLaneType, int wayCode, int fromStationID, int toStationID) {
         SubwaySchedule.OrdList ordList = getOrdListByDayOfWeek(dayOfWeek, subwaySchedule);
         List<SubwaySchedule.OrdList.TimeDirection.TimeData> time = getTimeByWayCode(wayCode, ordList);
-
-        //매시간마다
-        for (int i = time.size() - 1; i >= 0; i--) {
-            SubwaySchedule.OrdList.TimeDirection.TimeData timeData = time.get(i);
-            List<TimeInfo> timeInfos = TimeInfo.parseTimeString(timeData.getList());
-
-            AtomicBoolean canGoInSubway = new AtomicBoolean(false);
-            List<CompletableFuture<Void>> tasks = new ArrayList<>();
-
-            AtomicReference<TimeInfo> result = new AtomicReference<>();
-            //분마다
-            for (int j = timeInfos.size() - 1; j >= 0; j--) {
-                TimeInfo timeInfo = timeInfos.get(j);
-
-                //해당 호선의 지하철 노선도 데이터 가져오기
-                CompletableFuture<Void> task = new CompletableFuture<>();
-                LineMap lineMap = lineMapRepository.findByLineNum(odsayLaneType)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.NOT_FOUND_LINE_MAP));
-
-                int startIndex = -1;
-                int endIndex = -1;
-                int terminalIndex = -1;
-
-                List<LineStation> stationList = new ArrayList<>();
-                if (wayCode == UPTOWN) {
-                    stationList = lineMap.getUpLineList();
-                } else if (wayCode == DOWNTOWN) {
-                    stationList = lineMap.getDownLineList();
-                } else {
-                    throw new GeneralException(ErrorStatus.NOT_FOUND_LINE_STATION);
-                }
-
-                //순회하면서 출발역, 도착역, 종착역의 index구하기
-                for (int k = 0; k < stationList.size(); k++) {
-                    LineStation lineStation = stationList.get(k);
-
-                    String stationName = lineStation.getStationName();
-                    if (stationName.equals(timeInfo.getTerminalStation())) {
-                        terminalIndex = k;
-                    }
-
-                    int odsayStationID = lineStation.getOdsayStationId();
-                    if (odsayStationID == fromStationID && startIndex == -1) {
-                        startIndex = k;
-                    }
-
-                    //순환 경로인 경우 도착역은 뒤의 인덱스로 바뀌게
-                    if (odsayStationID == toStationID) {
-                        endIndex = k;
-                    }
-                }
-
-                //index가 출발역, 도착역, 종착역순이면 해당 열차를 탈 수 있다.
-                if (startIndex < endIndex && endIndex <= terminalIndex) {
-                    System.out.println("MAKAR " + "노선도 인덱스 확인: " + timeInfo.getMinute() + "분에 출발역(" + startIndex + ") 도착역(" + endIndex + ") 종착역(" + terminalIndex + ")");
-
-                    canGoInSubway.set(true);
-                    result.set(timeInfo);
-                    task.complete(null);
-                    return;
-                }
-
-                tasks.add(task);
-                CompletableFuture<Void> allOf = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0]));
-                try {
-                    allOf.get();
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-
-                if (canGoInSubway.get()) {
-                    //현재 날짜
-                    Calendar nowCalendar = Calendar.getInstance();
-                    Calendar makarCalendar = Calendar.getInstance();
-
-                    int makarHour = time.get(i).getIdx();
-                    int makarMinute = result.get().getMinute();
-
-                    int nowHour = nowCalendar.get(Calendar.HOUR_OF_DAY);
-                    int nowMinute = nowCalendar.get(Calendar.MINUTE);
-
-                    //막차시간이 시간표 상 24, 25인 경우
-                    if (makarHour >= 24) {
-                        makarHour -= 24;
-                        //현재 시간이 오전 3시를 넘으면
-                        if (nowHour >= 3) {
-                            //하루를 더함
-                            makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
-                        }
-                        //현재 시간이 오전 12시 ~ 오전 2시(새벽)면 하루를 안더함
-                    }
-
-                    makarCalendar.set(Calendar.HOUR_OF_DAY, makarHour);
-                    makarCalendar.set(Calendar.MINUTE, makarMinute);
-                    makarCalendar.set(Calendar.SECOND, 0);
-                    makarCalendar.set(Calendar.MILLISECOND, 0);
-
-                    //막차시간이 이미 지나간 경우 하루를 더함
-                    if (nowCalendar.after(makarCalendar)) {
-                        makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
-                    }
-                    return makarCalendar;
-                }
-            }
-        }
-        return null;
+        return findMakarTime(time, null, odsayLaneType, wayCode, fromStationID, toStationID);
     }
 
-    public Calendar computeTransferMakarTime(Calendar takingTime, int dayOfWeek, SubwaySchedule subwaySchedule,
-                                             int odsayLaneType, int wayCode, int fromStationID, int toStationID) {
-
+    // 환승이 있는 경우 막차 시간 구하기
+    public Calendar computeTransferMakarTime(Calendar takingTime, int dayOfWeek, SubwaySchedule subwaySchedule, int odsayLaneType, int wayCode, int fromStationID, int toStationID) {
         SubwaySchedule.OrdList ordList = getOrdListByDayOfWeek(dayOfWeek, subwaySchedule);
         List<SubwaySchedule.OrdList.TimeDirection.TimeData> time = getTimeByWayCode(wayCode, ordList);
+        return findMakarTime(time, takingTime, odsayLaneType, wayCode, fromStationID, toStationID);
+    }
 
-        //매시간마다
+    private Calendar findMakarTime(List<SubwaySchedule.OrdList.TimeDirection.TimeData> time, Calendar takingTime, int odsayLaneType, int wayCode, int fromStationID, int toStationID) {
         for (int i = time.size() - 1; i >= 0; i--) {
-
             SubwaySchedule.OrdList.TimeDirection.TimeData timeData = time.get(i);
 
-            //타야하는 시간보다 hour이 크면 skip
-            if (isBeforeTakingHour(timeData, takingTime)) {
+            if (takingTime != null && isBeforeTakingHour(timeData, takingTime)) {
                 continue;
             }
 
-            String list = timeData.getList();
-            List<TimeInfo> timeInfos = TimeInfo.parseTimeString(list);
-
+            List<TimeInfo> timeInfos = TimeInfo.parseTimeString(timeData.getList());
             AtomicBoolean canGoInSubway = new AtomicBoolean(false);
+            AtomicReference<TimeInfo> result = new AtomicReference<>();
             List<CompletableFuture<Void>> tasks = new ArrayList<>();
 
-            AtomicReference<TimeInfo> result = new AtomicReference<>();
-            //분마다
             for (int j = timeInfos.size() - 1; j >= 0; j--) {
                 TimeInfo timeInfo = timeInfos.get(j);
 
-                if (isBeforeTakingTime(timeData, timeInfo, takingTime)) {
+                if (takingTime != null && isBeforeTakingTime(timeData, timeInfo, takingTime)) {
                     continue;
                 }
 
-                //해당 호선의 지하철 노선도 데이터 가져오기
-                CompletableFuture<Void> task = new CompletableFuture<>();
-                LineMap lineMap = lineMapRepository.findByLineNum(odsayLaneType)
-                        .orElseThrow(() -> new GeneralException(ErrorStatus.NOT_FOUND_LINE_MAP));
+                CompletableFuture<Void> task = CompletableFuture.runAsync(() -> {
+                    try {
+                        LineMap lineMap = lineMapRepository.findByLineNum(odsayLaneType)
+                                .orElseThrow(() -> new GeneralException(ErrorStatus.NOT_FOUND_LINE_MAP));
 
+                        List<LineStation> stationList = getStationListByWayCode(lineMap, wayCode);
+                        int startIndex = getStationIndex(stationList, fromStationID);
+                        int endIndex = getStationIndex(stationList, toStationID);
+                        int terminalIndex = getTerminalIndex(stationList, timeInfo.getTerminalStation());
 
-                int startIndex = -1;
-                int endIndex = -1;
-                int terminalIndex = -1;
-
-                List<LineStation> stationList = new ArrayList<>();
-                if (wayCode == UPTOWN) {
-                    stationList = lineMap.getUpLineList();
-                } else if (wayCode == DOWNTOWN) {
-                    stationList = lineMap.getDownLineList();
-                } else {
-                    throw new GeneralException(ErrorStatus.NOT_FOUND_LINE_STATION);
-                }
-
-                //순회하면서 출발역, 도착역, 종착역의 index구하기
-                for (int k = 0; k < stationList.size(); k++) {
-                    LineStation lineStation = stationList.get(k);
-                    String stationName = lineStation.getStationName();
-                    if (stationName.equals(timeInfo.getTerminalStation())) {
-                        terminalIndex = k;
+                        if (startIndex < endIndex && endIndex <= terminalIndex) {
+                            System.out.println("MAKAR: 노선도 인덱스 확인: " + timeInfo.getMinute() + "분에 출발역(" + startIndex + ") 도착역(" + endIndex + ") 종착역(" + terminalIndex + ")");
+                            canGoInSubway.set(true);
+                            result.set(timeInfo);
+                        }
+                    } catch (GeneralException e) {
+                        e.printStackTrace();
                     }
-
-                    int odsayStationID = lineStation.getOdsayStationId();
-                    if (odsayStationID == fromStationID && startIndex == -1) {
-                        startIndex = k;
-                    }
-
-                    //순환 경로인 경우 도착역은 뒤의 인덱스로 바뀌게
-                    if (odsayStationID == toStationID) {
-                        endIndex = k;
-                    }
-                }
-
-                //index가 출발역, 도착역, 종착역순이면 해당 열차를 탈 수 있다.
-                if (startIndex < endIndex && endIndex <= terminalIndex) {
-                    System.out.println("MAKAR" + "노선도 인덱스 확인: " + timeInfo.getMinute() + "분에 출발역(" + startIndex + ") 도착역(" + endIndex + ") 종착역(" + terminalIndex + ")");
-                    canGoInSubway.set(true);
-                    result.set(timeInfo);
-                    task.complete(null);
-                    return;
-                }
+                });
 
                 tasks.add(task);
-                CompletableFuture<Void> allOf = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0]));
-                try {
-                    allOf.get();
-                } catch (InterruptedException |
-                         ExecutionException e) {
-                    e.printStackTrace();
-                }
+            }
 
-                if (canGoInSubway.get()) {
-                    //현재 날짜
-                    Calendar nowCalendar = Calendar.getInstance();
-                    Calendar makarCalendar = Calendar.getInstance();
+            waitForTasks(tasks);
 
-                    int makarHour = time.get(i).getIdx();
-                    int makarMinute = result.get().getMinute();
-
-                    int nowHour = nowCalendar.get(Calendar.HOUR_OF_DAY);
-                    int nowMinute = nowCalendar.get(Calendar.MINUTE);
-
-                    //막차시간이 시간표 상 24, 25인 경우
-                    if (makarHour >= 24) {
-                        makarHour -= 24;
-                        //현재 시간이 오전 3시를 넘으면
-                        if (nowHour >= 3) {
-                            //하루를 더함
-                            makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
-                        }
-                        //현재 시간이 오전 12시 ~ 오전 2시면 하루를 안더함
-                    }
-
-                    makarCalendar.set(Calendar.HOUR_OF_DAY, makarHour);
-                    makarCalendar.set(Calendar.MINUTE, makarMinute);
-                    makarCalendar.set(Calendar.SECOND, 0);
-                    makarCalendar.set(Calendar.MILLISECOND, 0);
-
-                    //막차시간이 이미 지나간 경우 하루를 더함
-                    if (nowCalendar.after(makarCalendar)) {
-                        makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
-                    }
-                    return makarCalendar;
-                }
+            if (canGoInSubway.get()) {
+                return computeMakarCalendar(timeData.getIdx(), result.get().getMinute());
             }
         }
         return null;
     }
+
+    private List<LineStation> getStationListByWayCode(LineMap lineMap, int wayCode) {
+        if (wayCode == UPTOWN) {
+            return lineMap.getUpLineList();
+        } else if (wayCode == DOWNTOWN) {
+            return lineMap.getDownLineList();
+        } else {
+            throw new GeneralException(ErrorStatus.NOT_FOUND_LINE_STATION);
+        }
+    }
+
+    private int getStationIndex(List<LineStation> stationList, int stationID) {
+        for (int k = 0; k < stationList.size(); k++) {
+            if (stationList.get(k).getOdsayStationId() == stationID) {
+                return k;
+            }
+        }
+        return -1;
+    }
+
+    private int getTerminalIndex(List<LineStation> stationList, String terminalStation) {
+        for (int k = 0; k < stationList.size(); k++) {
+            if (stationList.get(k).getStationName().equals(terminalStation)) {
+                return k;
+            }
+        }
+        return -1;
+    }
+
+    private void waitForTasks(List<CompletableFuture<Void>> tasks) {
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0]));
+        try {
+            allOf.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    private Calendar computeMakarCalendar(int makarHour, int makarMinute) {
+        Calendar nowCalendar = Calendar.getInstance();
+        Calendar makarCalendar = Calendar.getInstance();
+
+        int nowHour = nowCalendar.get(Calendar.HOUR_OF_DAY);
+        int nowMinute = nowCalendar.get(Calendar.MINUTE);
+
+        if (makarHour >= 24) {
+            makarHour -= 24;
+            if (nowHour >= 3) {
+                makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
+            }
+        }
+
+        makarCalendar.set(Calendar.HOUR_OF_DAY, makarHour);
+        makarCalendar.set(Calendar.MINUTE, makarMinute);
+        makarCalendar.set(Calendar.SECOND, 0);
+        makarCalendar.set(Calendar.MILLISECOND, 0);
+
+        if (nowCalendar.after(makarCalendar)) {
+            makarCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        return makarCalendar;
+    }
+
 
     //요일에 맞는 시간표 가져오기
     private SubwaySchedule.OrdList getOrdListByDayOfWeek(int dayOfWeek, SubwaySchedule subwaySchedule) {
