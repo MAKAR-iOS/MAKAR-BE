dbcc124afc7f8b449e3a06681206943eaae1fc40
chaeyeon0130 <k84292330@gmail.com>
2024-07-28 23:38:46 +0900

    chore: build.gradle 수정2

--- src/test/java/makar/dev/DevApplicationTests.java
+++ src/test/java/makar/dev/DevApplicationTests.java
@@ -3,7 +3,7 @@ package makar.dev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DevApplicationTests {
 
 	@Test
