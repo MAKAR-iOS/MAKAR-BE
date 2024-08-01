2694a02503f3e78f02a4430225dc31a4a08039ca
chaeyeon0130 <k84292330@gmail.com>
2024-08-01 20:55:26 +0900

    feat: timezone KST로 설정

--- src/main/java/makar/dev/DevApplication.java
+++ src/main/java/makar/dev/DevApplication.java
@@ -1,12 +1,21 @@
 package makar.dev;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class DevApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각 : " + new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevApplication.class, args);
